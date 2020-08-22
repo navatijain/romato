436510d0995d2d26bfe02aa87ea8f81339160877
Navati Jain <>
2020-08-21 20:11:07 -0400

    initial commit

---
+++ RomatoTests/RomatoTests.swift
@@ -0,0 +1,33 @@
+//
+//  RomatoTests.swift
+//  RomatoTests
+//
+//  Created by Navati Jain on 2020-08-21.
+//
+
+import XCTest
+@testable import Romato
+
+class RomatoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
